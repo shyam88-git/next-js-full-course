ef65170258a2e26f1b5dc95d3471f2f40525ed3c
Shyam Sharki <ssk66221@gmail.com>
2023-12-26 14:18:58 +0545

    first-commit

---
+++ next-js-crash-course/app/posts/new/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const newpost:React.FC = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default newpost;
---
+++ next-js-crash-course/app/posts/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const post:React.FC = () => {
+  return (
+
+     <div>Post</div>
+  )
+}
+
+export default post;
